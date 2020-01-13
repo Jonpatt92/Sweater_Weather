eef1d782f05d87a9d356f2074fbb2c6eea3ffdfb
Jon Patterson <JonPatt92@gmail.com>
2020-01-13 12:44:16 -0700

    Update forecast poro to have methods needed for json output

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,7 +1,38 @@
 class Forecast
   attr_reader :current, :hourly
+
   def initialize(weather_data)
+    binding.pry
     @current = weather_data[:currently]
     @hourly  = weather_data[:hourly]
+    @daily   = weather_data[:daily]
+  end
+
+  def summary
+    binding.pry
+  end
+
+  def high
+    binding.pry
+  end
+
+  def low
+    binding.pry
+  end
+
+  def feels_like
+    binding.pry
+  end
+
+  def humidity
+    binding.pry
+  end
+
+  def visibility
+    binding.pry
+  end
+
+  def uv_index
+    binding.pry
   end
 end
