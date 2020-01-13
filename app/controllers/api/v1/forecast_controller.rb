b4abbf6119d5bc00997bbb6859d42cd2b3c5da08
Jon Patterson <JonPatt92@gmail.com>
2020-01-13 12:44:01 -0700

    Update forecast controller to have json output required by frontend

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,10 +1,32 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    location = LocationFacade.new(params[:location])
-    forecast = ForecastFacade.new(location.coordinates)
-    forecast.weather_data
-    # render locals: {
+    location_facade = LocationFacade.new(params[:location])
+    forecast = ForecastFacade.new(location_facade.coordinates).forecast
+    location = location_facade.location
 
-    # }
+    render json: {
+      type: "Forecast",
+      location: {
+         original_search: params[:location],
+           location_city: location.city,
+          location_state: location.state,
+        location_country: location.country
+      },
+      current_forecast: {
+           summary: forecast.summary,
+              high: forecast.high,
+               low: forecast.low,
+        feels_like: forecast.feels_like,
+          humidity: forecast.humidity,
+        visibility: forecast.visibility,
+          uv_index: forecast.uv_index
+      },
+      extended_forecast: {
+
+      },
+      hourly_forecast: {
+
+      }
+    }
   end
 end
