55d6d2524111ab63cfa5d49282daf1af0533ff14
Jon Patterson <JonPatt92@gmail.com>
2020-01-14 14:57:09 -0700

    Replace hardcoded JSON with serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,11 +2,6 @@ class Api::V1::ForecastController < ApplicationController
   def show
     forecast_facade = ForecastFacade.new(params[:location])
 
-    render json: {
-      type: "Forecast",
-      original_search: params[:location],
-      location: forecast_facade.location,
-      forecast: forecast_facade.forecast
-    }
+    render json: ForecastSerializer.new(forecast_facade.forecast)
   end
 end
