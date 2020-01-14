7e72621ade8ad8e21d372510d4a70627daa7f786
Jon Patterson <JonPatt92@gmail.com>
2020-01-13 17:44:09 -0700

    Create instance variables to expose needed data. Create functions to create daily_forecast and hourly_forecast poros. Create get_offset_time method to calculate and format time local to requested location

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,38 +1,35 @@
 class Forecast
-  attr_reader :current, :hourly
+  attr_reader :current_time, :hourly_forecast, :daily_forecast, :current_summary, :day_summary, :temperature, :high, :low, :feels_like, :humidity, :visibility, :uv_index
 
   def initialize(weather_data)
-    binding.pry
-    @current = weather_data[:currently]
-    @hourly  = weather_data[:hourly]
-    @daily   = weather_data[:daily]
+    @current_time    = get_offset_time(weather_data[:currently][:time], weather_data[:timezone])
+    @current_summary = weather_data[:currently][:summary]
+    @day_summary     = weather_data[:daily][:data][0][:summary]
+    @temperature     = weather_data[:currently][:temperature]
+    @todays_high     = weather_data[:daily][:data][0][:temperatureHigh]
+    @todays_low      = weather_data[:daily][:data][0][:temperatureLow]
+    @feels_like      = weather_data[:currently][:apparentTemperature]
+    @humidity        = weather_data[:currently][:humidity]
+    @visibility      = weather_data[:currently][:visibility]
+    @uv_index        = weather_data[:currently][:uvIndex]
+    @hourly_forecast = create_hourly_forecast(weather_data[:hourly][:data][0..7], weather_data[:timezone])
+    @daily_forecast  = create_daily_forecast(weather_data[:daily][:data][0..4], weather_data[:timezone])
   end
 
-  def summary
-    binding.pry
+  def create_hourly_forecast(hourly_data, timezone)
+    hourly_data.map do |hour_data|
+      HourlyForecast.new(hour_data, timezone)
+    end
   end
 
-  def high
-    binding.pry
+  def create_daily_forecast(daily_data, timezone)
+    daily_data.map do |day_data|
+      DailyForecast.new(day_data, timezone)
+    end
   end
 
-  def low
-    binding.pry
-  end
-
-  def feels_like
-    binding.pry
-  end
-
-  def humidity
-    binding.pry
-  end
-
-  def visibility
-    binding.pry
-  end
-
-  def uv_index
-    binding.pry
+  def get_offset_time(time, timezone)
+    tz = TZInfo::Timezone.get(timezone)
+    Time.at(time, in: tz).strftime('%l:%M %p, %-m/%d')
   end
 end
