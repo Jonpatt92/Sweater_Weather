d4124ef8425ca6746dbd34826e6f4d58c182a362
Jon Patterson <JonPatt92@gmail.com>
2020-01-14 19:36:08 -0700

    Create request spec for user login

---
+++ spec/requests/user_login_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'When a returning user submits email and password' do
+  before(:all) do
+    @user = User.create(email: 'email@example.com', password: "password", password_confirmation: "password")
+  end
+
+  it 'Can return their api key' do
+    post "/api/v1/sessions", :params => {email: "email@example.com", password: "password" }
+
+    expect(response.status).to eq(201)
+    expect(response.body).to include(@user.api_key)
+  end
+
+  it 'Can refuse access for incorrect credentials' do
+    post "/api/v1/sessions", :params => {email: "email@example.com", password: "wrong_pass" }
+
+    expect(response.status).to eq(401)
+    expect(response.body).to_not include(@user.api_key)
+  end
+end
