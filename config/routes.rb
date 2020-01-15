56d30fd098b68b55729f7dc4ea92b5aa6cffc958
Jon Patterson <JonPatt92@gmail.com>
2020-01-15 10:58:39 -0700

    Add backgrounds route for image lookup

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/forecast',   to: 'forecast#show'
-      post '/users',     to: 'user#create'
-      post '/sessions',  to: 'user#show'
-      post '/road_trip', to: 'trip#create'
+      get '/forecast',    to: 'forecast#show'
+      post '/users',      to: 'user#create'
+      post '/sessions',   to: 'user#show'
+      post '/road_trip',  to: 'trip#create'
+      get '/backgrounds', to: 'image#show'
     end
   end
 end
